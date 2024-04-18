ae5d43dd6acacc260102060afc3199f8aa2d5ab3
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-04-18 12:14:11 -0300

    Modificando script

--- ScriptBD-MindCore.sql
+++ ScriptBD-MindCore.sql
@@ -2,9 +2,8 @@ create database MindCore;
 use MindCore;
 
 create table Empresa (
-idEmpresa int primary key auto_increment,
+cnpj char(14) primary key unique,
 nome varchar(45),
-cnpj char(14),
 telefone char(11));
 
 create table Estoque (
@@ -12,33 +11,34 @@ idEstoque int primary key auto_increment,
 componente varchar(45),
 quantidade int,
 preco decimal,
-fkEmpresa int,
-foreign key (fkEmpresa) references Empresa(idEmpresa)
+fkEmpresa char(14),
+foreign key (fkEmpresa) references Empresa(cnpj)
 );
 create table Sala (
 idSala int primary key auto_increment,
 nome varchar(45),
 andar int,
-fkEmpresa int,
-foreign key (fkEmpresa) references Empresa(idEmpresa)
+fkEmpresa char(14),
+foreign key (fkEmpresa) references Empresa(cnpj)
 );
 create table Funcionario (
 idFunc int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
+senha varchar(45),
 telefone char(11),
 tipo varchar(45),
-check (tipo in('admin','comum')),	
-fkEmpresa int,
-foreign key (fkEmpresa) references Empresa(idEmpresa)
+check (tipo in('empresa','admin','comum')),	
+fkEmpresa char(14),
+foreign key (fkEmpresa) references Empresa(cnpj)
 );
 create table Maquina(
 idMaquina int primary key auto_increment,
 ip varchar(45),
 fkSala int,
-fkEmpresa int,
+fkEmpresa char(14),
 foreign key (fkSala) references Sala(idSala),
-foreign key (fkEmpresa) references Empresa(idEmpresa));
+foreign key (fkEmpresa) references Empresa(cnpj));
 
 -- CRIAR TABELAS PARA CADA HARDWARE, COM SUAS RESPECTIVAS INFORMAÇÕES!!!
 
@@ -60,6 +60,15 @@ sistemaOperacional varchar(100),
 memoriaUso Long,
 discoUso Long,
 fkMaquina int);
+select * from empresa;
+select * from funcionario;
+
+
+
+        
+
+
+
 
 
 
